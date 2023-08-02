a2ca83bb437e3b05d8d743bde6dd5e115a01f966
Roman K <roman@openixsolutions.com>
2023-08-02 13:29:36 -0400

    okay

---
+++ colab_py_script.py
@@ -0,0 +1,13 @@
+# list working directory?
+!pwd
+# makes directory
+!mkdir roman
+# basic loop
+for i in range (6):
+    print (i)
+# starts here:
+# import pandas as pd
+import numpy as np
+import matplotlib.pylab as plt
+import seaborn as sns
+    
