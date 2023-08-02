1aeb1a9838ec5113391aa99035931914bd7f82e0
Roman K <roman@openixsolutions.com>
2023-08-02 13:41:14 -0400

    edits

---
+++ py_code_snippets.py
@@ -0,0 +1,3 @@
+# basic loop
+for i in range (6):
+    print (i)
---
+++ py_handy_commands.py
@@ -0,0 +1,13 @@
+# list working directory?
+!pwd
+# makes directory
+!mkdir roman
+df.shape
+df.head()
+df.columns
+df.dtypes
+df.describe()
+df.isna().sum()
+df.duplicated()
+df.loc[df.duplicated()]
+df.query('last_name == "Mumbey"')
