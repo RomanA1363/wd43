111b9790bece2efc1431d18600a0b70842ad8b2f
Roman K <roman@openixsolutions.com>
2023-08-02 13:36:08 -0400

    second commit

--- colab_py_script.py
+++ colab_py_script.py
@@ -10,4 +10,16 @@ for i in range (6):
 import numpy as np
 import matplotlib.pylab as plt
 import seaborn as sns
-    
+plt.style.use('ggplot')
+pd.set_option('display.max_columns' , 200)
+df = pd.read_csv('/CoLab-Dataset-1-100K.csv')
+df.shape
+df.head()
+df.columns
+df.dtypes
+df.describe()
+df.isna().sum()
+df.duplicated()
+df.loc[df.duplicated()]
+df.query('last_name == "Mumbey"')
+
