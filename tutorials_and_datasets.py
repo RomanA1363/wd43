fa4e1b0c8d5734b85a7b31cdb26107282a94e2c8
Roman K <roman@openixsolutions.com>
2023-08-06 20:55:02 -0400

    changes

--- tutorials_and_datasets.py
+++ tutorials_and_datasets.py
@@ -1,3 +1,4 @@
 # https://www.youtube.com/watch?v=xi0vhXFPegw
 # https://www.mockaroo.com/schemas/548489
 # https://devinschumacher.com/how-to-combine-merge-multiple-csv-or-excel-files-for-mac-pc/
+# https://www.statology.org/pandas-convert-object-to-int/
