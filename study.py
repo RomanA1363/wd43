945ed5a958e7c909fb3e2991354cb53eb3049dc7
Roman K <roman@openixsolutions.com>
2023-08-12 19:45:15 -0400

    c

--- study.py
+++ study.py
@@ -10,6 +10,12 @@ from faker import Faker
 
 os.system("clear")
 
+SendingOrg = ['Sending Organization 1','Sending Organization 2','Sending Organization 3','Sending Organization 4','Sending Organization 5']
+ReceivingOrg = ['Receiving Organization 1','Receiving Organization 2','Receiving Organization 3','Receiving Organization 4','Receiving Organization 5']
+CommPoint = ['Comm Point 1','Comm Point  2','Comm Point  3','Comm Point  4','Comm Point  5']
+PayloadType = ['HL7','CCDA','FHIR','CSV']
+verato_exception = ['','','','','','','exception 1','','','','','','','','','','','','','','','','','','','','','exception 2','','','','','','','','','',]
+
 adt_hl7_types = ["ADT_A01", "ADT_A02", "ADT_A03", "ADT_A04", "ADT_A05", "ADT_A06", "ADT_A08", "ADT_A12", "ADT_A15", "ADT_A16"]
 gender = ['M','F']
 TF = ['T','F']
@@ -38,8 +44,8 @@ telemetry = {
   "root_id": ''+str(uuid.uuid4())+'',
   "verato_id": ''+str(uuid.uuid4())+'',
   "msh_7": ''+str(gen_datetime(2023, 2023))+'',
-  "msh_4": ''+fake.company()+'', # MSH.4 - Sending Facility
-  "msh_6": ''+fake.company()+'', #  MSH.6 - Receiving Facility
+  "msh_4": ''+random.choice(SendingOrg)+'', # MSH.4 - Sending Facility
+  "msh_6": ''+random.choice(ReceivingOrg)+'', #  MSH.6 - Receiving Facility
   "msh_9_1": "ADT",
   "msh_9_2": ''+random.choice(adt_hl7_types)+'',
   "msh_10": ''+generate_random_alphanumeric_string(15)+'',
@@ -55,7 +61,12 @@ telemetry = {
   "obr_3": ''+generate_random_alphanumeric_string(10)+'',
   "obr_24": ''+generate_random_alphanumeric_string(10)+'',
   "Part2": ''+random.choice(TF)+'',
-  "SDOH": ''+random.choice(TF)+'' 
+  "SDOH": ''+random.choice(TF)+'',
+  "inbound_comm_point": ''+random.choice(CommPoint)+'',
+  "verato_exception": ''+random.choice(verato_exception)+'',
+  "original_file_name": ''+str(uuid.uuid4())+'.hl7',
+  "archived_file_name": ''+str(uuid.uuid4())+'',
+  "payload_type": ''+random.choice(PayloadType)+''
 }
 
 
