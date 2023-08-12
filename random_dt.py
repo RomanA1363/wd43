85f25dfa6fd228590035115604800efd82ec666f
Roman K <roman@openixsolutions.com>
2023-08-12 11:25:32 -0400

    commit

--- random_dt.py
+++ random_dt.py
@@ -1,10 +1,22 @@
-import os
-import uuid
+
 import random
+import os
 from datetime import datetime, timedelta
 
+os.system("clear")
 
+min_year=2023
+max_year=datetime.now().year
 
+start = datetime(min_year, 1, 1, 00, 00, 00)
+years = max_year - min_year+1
+end = start + timedelta(days=365 * years)
+
+for i in range(10):
+    random_date = start + (end - start) * random.random()
+    print(random_date)
+
+#done
 
 # or a function
 def gen_datetime(min_year, max_year):
@@ -14,15 +26,4 @@ def gen_datetime(min_year, max_year):
     end = start + timedelta(days=365 * years)
     return start + (end - start) * random.random()
 
-
-os.system("clear")
-telemetry = {
-  "id": uuid.uuid4(),
-  "transaction_dts": gen_datetime(2023, 2023),
-  "age": 300,
-  "skills": ["coding", "language modeling", "natural language processing"]
-}
-telemetry['age']=1555
-#print(ts)
-for key, value in telemetry.items():
-  print(key, value)
+ 
